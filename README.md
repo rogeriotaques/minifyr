8b4039c95481829befc634c324312d9b4747b2fb
Rogerio Taques <rogerio@jarvispro.local>
2015-10-21 02:11:49 +0900

    1.5 > 1.6

--- README.md
+++ README.md
@@ -1,37 +1,35 @@
-# Minifyr 1.2 beta
+# Minifyr 1.6 beta
 
 A PHP script that minify and group CSS and JS scripts.
 
-You should use it in order to save bandwidth and accelerate load time for your site or web-app.
+You may be interested on save bandwith and reduce the load time of your website or application, then, **Minifyr** is good for you.
+
+From version 1.6 it supports external files and prevent double minification on already minified files.
 
 See the examples below:
 
-   [Not minified](http://awin.com.br/assets/css/icomoon.css) | 
-   [Minified](http://awin.com.br/minifyr.php?f=assets/css/icomoon.css&screen) | 
-   [Minified (forcing download)](http://awin.com.br/minifyr.php?f=assets/css/icomoon.css&screen) | 
+   [Not minified](http://awin.com.br/assets/css/icomoon.css) |
+   [Minified](http://awin.com.br/minifyr.php?f=assets/css/icomoon.css&screen) |
+   [Minified (forcing download)](http://awin.com.br/minifyr.php?f=assets/css/icomoon.css&screen) |
    [Debug mode](http://awin.com.br/minifyr.php?f=assets/css/icomoon.css&screen&debug)
 
 ## How to use
 
-Download this project, copy and paste the "minifyr.php" file into any folder of your project. I strongly 
+Download this project, copy and paste the "minifyr.php" file into any folder of your project. I strongly
 suggest that you paste it in root directory, this way it becomes easy to refer.
 
 For example:
 
 
 ```
-
-   / (project root directory)
-   /minifyr.php
-
+/ (project root directory)
+/minifyr.php
 ```
 
 Now, everything you have to do is call it in your HTML file:
 
 ```
-
 <link type="text/css" media="all" href="minifyr.php?f=path/to/css/file.css" />
-
 ```
 
 That's it. Easy and simple. A piece of cake! :)
@@ -46,9 +44,7 @@ The path of file you'd like to load and minify.
 
 
 ```
-
-   minifyr.php?f=assets/css/my-css-file.css
-
+minifyr.php?f=assets/css/my-css-file.css
 ```
 
 You can also pass a list of files. In this case, all files will be loaded and will be returned minified as a unique file. This technique is interesting to reduces the number of calls you make to your server.
@@ -56,9 +52,13 @@ You can also pass a list of files. In this case, all files will be loaded and wi
 To pass a list of files, you should give file names separated by comma (,):
 
 ```
+minifyr.php?f=assets/css/my-css-file-1.css,assets/css/my-css-file-2.css,...
+```
 
-   minifyr.php?f=assets/css/my-css-file-1.css,assets/css/my-css-file-2.css,...
+Whenever it's necessary to load external files, you can just pass the file with a prefix ```external|```.
 
+```
+minifyr.php?f=external|code.jquery.com/jquery-2.1.1.min.js[, ...]
 ```
 
 ### screen: void
@@ -71,9 +71,7 @@ Just add it in the end of your query string:
 
 
 ```
-
-   minifyr.php?f=assets/css/my-css-file.css&screen
-
+minifyr.php?f=assets/css/my-css-file.css&screen
 ```
 
 ### debug: void
@@ -86,9 +84,7 @@ Just add it in the end of your query string:
 
 
 ```
-
-   minifyr.php?f=assets/css/my-css-file.css&debug
-
+minifyr.php?f=assets/css/my-css-file.css&debug
 ```
 
 [![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/rogeriotaques/minifyr/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
--- minifyr.php
+++ minifyr.php
@@ -1,56 +1,56 @@
 <?php
 
 /**
- * Minifyr - A minifier PHP script for CSS and JS scripts.  
- * Copyright (c) 2014, Rogério Taques. 
+ * Minifyr - A minifier PHP script for CSS and JS scripts.
+ * Copyright (c) 2014, Rogério Taques.
  *
  * Licensed under MIT license:
  * http://www.opensource.org/licenses/mit-license.php
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
- * software and associated documentation files (the "Software"), to deal in the Software 
- * without restriction, including without limitation the rights to use, copy, modify, merge, 
- * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
  * to whom the Software is furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all copies or 
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
- * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * @requires PHP4+
  * @author Rogério Taques (rogerio.taques@gmail.com)
  * @see https://github.com/rogeriotaques/minifyr
- * 
- * @uses 
+ *
+ * @uses
  * 		minifyr.php?f=path/to/file.css ( force to download file )
  * 		minifyr.php?f=path/to/file.css&screen ( not force download )
  * 		minifyr.php?f=path/to/file.css&screen&debug ( do not minify and not force download )
  */
 
-$am_version = '1.5 beta';
+$am_version = '1.6 beta';
 
 /**
  * Do minify ...
  */
-function minify( $file_path, $content_type ) 
+function minify( $file_path, $content_type )
 {
 	// get file content
 	$content_file = @file_get_contents( $file_path );
-	
+
 	// remove all comment blocks
 	$content_file = preg_replace( '#/\*.*?\*/#s', '', $content_file );
-		
+
 	// remove all comment lines
 	$content_file = preg_replace( '#//(.*)$#m', '', $content_file );
-	
+
 	// remove all blank spaces
 	$content_file = preg_replace( '#\s+#', ' ', $content_file );
-	
+
 	// remove unecessary spaces (before|after) some signs ...
 	$content_file = str_replace( array('{ ',' {'), '{', $content_file );
 	$content_file = str_replace( array('} ',' }'), '}', $content_file );
@@ -59,20 +59,20 @@ function minify( $file_path, $content_type )
 	$content_file = str_replace( array(', ',' ,'), ',', $content_file );
 	$content_file = str_replace( array('|| ',' ||'), '||', $content_file );
 	$content_file = str_replace( array('! ',' !'), '!', $content_file );
-    
+
     // perform different ways to remove some unecesary spaces (before|after) some signs ...
     switch( $content_type )
     {
         case 'css':
-    
+
             $content_file = str_replace( array('( ',' ('), '(', $content_file );
             $content_file = str_replace( array(' )'), ')', $content_file );
             $content_file = str_replace( array('= ',' ='), '=', $content_file );
-        
+
             break;
-        
+
         case 'js' :
-    
+
             $content_file = str_replace( array('( ',' ('), '(', $content_file );
             $content_file = str_replace( array(' )',') '), ')', $content_file );
             $content_file = str_replace( array('= ',' ='), '=', $content_file );
@@ -80,10 +80,10 @@ function minify( $file_path, $content_type )
             $content_file = str_replace( array('- ',' -'), '-', $content_file );
             $content_file = str_replace( array('* ',' *'), '*', $content_file );
             $content_file = str_replace( array('/ ',' /'), '/', $content_file );
-        
+
             break;
     }
-    
+
 	return trim( $content_file );
 }
 
@@ -96,12 +96,12 @@ function path_fix( $content, $file_path )
 {
 	$content = preg_replace('/(\'|\"|\()(\.\.\/)/', "$1{$file_path}/$2", $content);
 	$content = preg_replace('/(url\()(\'|\"){0,1}([a-zA-Z0-9\-\_\.]+)(\.png|\.jpg|\.jpge|\.gif|\.bmp|\.PNG|\.JPG|\.JPEG|\.GIF|\.BMP])/', "$1$2{$file_path}/$3$4", $content);
-	 
+
 	return $content;
 }
 
-$allow = array('css','js');	// allowed file extensions
-$minified = array();		// a list of minified files
+$allow = array('css','js');		// allowed file extensions
+$minified = array();					// a list of minified files
 
 $content_types = array('css' => 'text/css', 'js' => 'text/javascript');
 $content_type  = null;
@@ -109,7 +109,7 @@ $content = '';
 
 // get settings and files to minify
 // options are:
-//   f		- Required. File or comma separated file list
+//   f			- Required. File or comma separated file list
 //	 screen	- Optional. Void. Forces the download of minified file.
 // 	 debug	- Optional. Void. When given, skip minification.
 
@@ -119,47 +119,58 @@ $files  = isset( $_GET[ 'f' ] ) ? $_GET[ 'f' ] : NULL;
 $files  = explode( ',', $files );
 
 $file_ext = null;
+$is_minified = false;
 
 foreach ($files as $file)
 {
 	// allow external files
+	// whenever it's an external file, load it from its source
 	$external = preg_match('/^external\|/', $file) ? TRUE : FALSE;
-	if ($external) $file = preg_replace('/^external\|/', '', $file); 
-	
+	if ($external) $file = preg_replace('/^external\|/', 'http://', $file);
+
 	$inf = pathinfo($file);
+	$is_minified = strpos($inf['basename'], '.min') !== false;
 
 	// ignore file if it's invalid or was minified already.
 	// files considered invalid are: not allowed extensions or with path pointing to parent folders (../)
 	if ( !$file || !in_array( $inf['extension'], $allow ) || strpos( $inf['dirname'], '../' ) !== false || in_array($inf['basename'], $minified) )
 	{
 		$content .= "/* File: {$file} was ignored. It's invalid or was minified already. */".PHP_EOL.PHP_EOL;
-		continue; 
+		continue;
 	}
-	
+
 	// decide the content type according first file type.
 	if (! $content_type)
 	{
 		$content_type = $content_types[ $inf['extension'] ];
 		$file_ext = $inf['extension'];
 	}
-	
+
 	// if extension isn't the same of first minified files, then ignore it
 	if ( $content_type != $content_type )
 	{
 		$content .= "/* File: {$file} was ignored. File's extension doesn't match file type pattern. */".PHP_EOL.PHP_EOL;
 		continue;
 	}
-	
-	
-	
-	// avoid minify it again ...
+
+
+
+	// prevent double minification ...
 	$minified[] = $file;
+
+	if (!$is_minified)
+	{
 	$minified_content = !$debug ? minify( $file, $content_type ) : @file_get_contents( $file );
 	$minified_content = $content_type == 'css' ? path_fix( $minified_content, $inf['dirname'] ) : $minified_content;
-	
+	}
+	else
+	{
+		$minified_content = @file_get_contents($file);
+	}
+
 	$content .= "/* File: {$file} */".PHP_EOL.PHP_EOL;
-	$content .= $minified_content.PHP_EOL.PHP_EOL;	
-	
+	$content .= $minified_content.PHP_EOL.PHP_EOL;
+
 }
 
 // Enable gzip compression
@@ -168,7 +179,7 @@ ob_start("ob_gzhandler");
 // Allow cache
 header('Cache-Control: public');
 
-// Expires in a day 
+// Expires in a day
 header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 86400) . ' GMT');
 
 // Set right content-type and charset ...
